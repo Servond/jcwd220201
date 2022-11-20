f9b14cf767aad4e413abe73a62c78615c7414212
fraya <fitrah.fraya@gmail.com>
2022-11-20 19:21:44 +0700

    profile page

---
+++ projects/client/src/pages/Profile.jsx
@@ -0,0 +1,20 @@
+import { axiosInstance } from "../api";
+import { useParams } from "react-router-dom";
+
+const ProfilePage = () => {
+  //     const params = useParams()
+  //   const fetchUser = async () => {
+  //     try {
+  //       const response = await axiosInstance.get("/users" {
+  //         // params: {
+  //         //     email: params.email,
+  //         //     password: params.password
+  //         }
+  //       });
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+};
+
+export default ProfilePage;
