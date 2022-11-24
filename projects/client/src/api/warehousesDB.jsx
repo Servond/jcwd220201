fdf73c8e469a9bb43dae8dbdbbecfdc3d97e9097
itsSimpliPhi <fidelhadibukhara@gmail.com>
2022-11-24 12:40:06 +0700

    G1-31 Fixing Conflict
    Fixing Conflict

--- projects/client/src/api/warehousesDB.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const axiosInstances = axios.create({
-  baseURL: "http://localhost:8000",
-})
-
-// Auth token check nanti disini tapi masih bingung
-
-export { axiosInstances }
