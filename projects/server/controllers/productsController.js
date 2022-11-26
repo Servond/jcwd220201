25c9e679065cb0e8550f4bfae9fbc43d4c5ddc99
RGS <nuradlifathan@gmail.com>
2022-11-26 12:27:57 +0700

    getAll and ById Product

---
+++ projects/server/controllers/productsController.js
@@ -0,0 +1,53 @@
+const db = require("../models")
+const { Op } = require("sequelize")
+const productsController = {
+  getAllProducts: async (req, res) => {
+    try {
+      const {
+        _limit = 10,
+        _page = 1,
+        _sortBy = "id",
+        _sortDir = "ASC",
+      } = req.query
+      const getAllProducts = await db.Product.findAndCountAll({
+        where: {
+          product_name: {
+            [Op.like]: `%${req.query.product_name || ""}%`,
+          },
+        },
+        // include: [{ models: db.categories }],
+        limit: Number(_limit),
+        offset: (_page - 1) * _limit,
+        order: [[_sortBy, _sortDir]],
+      })
+
+      return res.status(200).json({
+        message: "Get All Product",
+        data: getAllProducts.rows,
+        dataCount: getAllProducts.count,
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({
+        message: "Server Error! ❌",
+      })
+    }
+  },
+  getProductsByID: async (req, res) => {
+    try {
+      const { id } = req.params
+      const getProductsByID = await db.Product.findByPk(id)
+      res.status(200).json({
+        message: "Get Products By Id",
+        data: getProductsByID,
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({
+        message: "Server Error! ❌",
+      })
+    }
+  },
+}
+
+module.exports = productsController
---
+++ projects/server/routes/productsRoute.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const productsController = require("../controllers/productsController")
+const router = express.Router()
+
+router.get("/", productsController.getAllProducts)
+router.get("/:id", productsController.getProductsByID)
+
+module.exports = router
