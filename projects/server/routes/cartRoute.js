a08252d637ac6a6319c01a7a7a138e21c3009e15
RGS <nuradlifathan@gmail.com>
2022-12-09 02:18:53 +0700

    deleted

--- projects/server/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const cartController = require("../controllers/cartsController")
-
-const router = express.Router()
-
-router.post("/", cartController.addToCart)
-router.get("/", cartController.showCartItems)
-
-module.exports = router
