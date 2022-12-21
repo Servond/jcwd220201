2b2326774db1ca6bf0f7f02ce96895e8ff305bf7
RGS <nuradlifathan@gmail.com>
2022-12-21 10:36:58 +0700

    lib : stock pagination

---
+++ projects/server/lib/stock/paginationData.js
@@ -0,0 +1,9 @@
+const paginationData = (data, page, limit) => {
+  const { count: totalWarehouse, rows: Warehouse } = data
+  const currentPage = page ? page : 1
+  const totalPage = Math.ceil(totalWarehouse / limit)
+
+  return { totalWarehouse, Warehouse, totalPage, currentPage }
+}
+
+module.exports = paginationData
