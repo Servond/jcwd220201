55e0e79fa2d8ff77074ea35e7f29f7e2a575602d
fraya <fitrah.fraya@gmail.com>
2022-12-24 17:13:57 +0700

    ui order

--- projects/client/src/components/order/orderCard.jsx
+++ projects/client/src/components/order/orderCard.jsx
@@ -1,106 +1,177 @@
-// import {
-//   Box,
-//   Button,
-//   Flex,
-//   Grid,
-//   HStack,
-//   Stack,
-//   Text,
-//   Tr,
-// } from "@chakra-ui/react"
-// import { useEffect } from "react"
-// import { useState } from "react"
-// import { GrLocation } from "react-icons/gr"
-// import { IoMdArrowRoundBack } from "react-icons/io"
-// import { Link } from "react-router-dom"
-// import { axiosInstance } from "../../api"
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  HStack,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react"
+import { useEffect, useRef } from "react"
+import { useState } from "react"
+import { BsThreeDots } from "react-icons/bs"
 
-// import OrderCard from "./order"
+import { axiosInstance } from "../../api"
 
-// const Order = () => {
-//   const [order, setOrder] = useState([])
-//   const fetchOrder = async () => {
-//     try {
-//       const resp = await axiosInstance.get(`/order`)
+import OrderCard from "./order"
 
-//       setOrder(resp.data.data)
-//       renderOrder()
-//       console.log(setOrder, "orde")
-//     } catch (err) {
-//       console.log(err)
-//     }
-//     const cancelBtn = async (id) => {
-//       try {
-//         await axiosInstance.patch(`/order/cancel${id}`)
-//       } catch (err) {
-//         console.log(err)
-//       }
-//     }
+const Order = () => {
+  const [order, setOrder] = useState([])
+  const toast = useToast()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef()
+  const fetchOrder = async () => {
+    try {
+      const resp = await axiosInstance.get(`/order`)
 
-//     const renderOrder = () => {
-//       console.log(order, "order")
-//       return order.map((val) => {
-//         return (
-//           <OrderCard
-//             key={val.id.toString()}
-//             payment_date={val.payment_date}
-//             shipping_service={val.shipping_service}
-//             payment_reciept={val.payment_reciept}
-//             // onDelete={() => deleteBtnHandler(val.id)}
-//           />
-//         )
-//       })
-//     }
-//   }
+      setOrder(resp.data.data)
 
-//   useEffect(() => {
-//     fetchOrder()
-//   }, [])
+      console.log(setOrder, "orde")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  const cancelBtn = async (id) => {
+    try {
+      await axiosInstance.patch(`/order/cancel${id}`)
 
-//   return (
-//     <>
-//       <Stack>
-//         <Box
-//           display={"flex"}
-//           fontSize="14px"
-//           justifyContent={"center"}
-//           mt={"50px"}
-//         >
-//           <Box
-//             w="500px"
-//             boxShadow={"0 0 10px 3px rgb(0 0 0 / 10%)"}
-//             borderRadius={"10px"}
-//             p="24px 40px 32px "
-//             textAlign={"center"}
-//             bgColor={"white"}
-//             mt="120px"
-//           >
-//             <Text
-//               fontSize="22px"
-//               fontWeight={"bold"}
-//               textAlign={"left"}
-//               color="teal.600"
-//               fontFamily="Open Sauce One',sans-serif"
-//             >
-//               Belanja...
-//             </Text>
-//           </Box>
-//         </Box>
+      fetchOrder()
+      toast({
+        status: "success",
+        title: "Done",
+      })
+    } catch (err) {
+      console.log(err)
+      toast({
+        status: "error",
+        title: "Failed",
+      })
+    }
+  }
 
-//         <Grid
-//           templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(5, 1fr)" }}
-//           mt="4"
-//           minChildWidth="250px"
-//           // spacing="5em"
-//           gap="1em"
-//           minH="full"
-//           align="center"
-//         >
-//           {renderOrder()}
-//         </Grid>
-//       </Stack>
-//     </>
-//   )
-// }
+  const handleConfirmCancel = () => {
+    console.log(handleConfirmCancel, "can")
+    // onCancel()
+    onClose()
+  }
 
-// export default Order
+  const renderItem = () => {
+    return order.map((val) => {
+      return (
+        <Tr key={val.id}>
+          <Td>{val.payment_date}</Td>
+          <Td>{val.shipping_service}</Td>
+          <Td> {val.payment_reciept}</Td>
+          <Td> {val.total_price}</Td>
+          <Td onClick={() => cancelBtn(val.id)}></Td>
+
+          <Td>
+            <Menu>
+              <MenuButton>
+                <Icon as={BsThreeDots} boxSize="20px" />
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={onOpen}>Cancel</MenuItem>
+              </MenuList>
+            </Menu>
+          </Td>
+        </Tr>
+      )
+    })
+  }
+
+  useEffect(() => {
+    fetchOrder()
+  }, [])
+
+  return (
+    <>
+      <Stack>
+        <Box
+          display={"flex"}
+          fontSize="14px"
+          justifyContent={"center"}
+          mt={"50px"}
+        >
+          <Box
+            w="500px"
+            boxShadow={"0 0 10px 3px rgb(0 0 0 / 10%)"}
+            borderRadius={"10px"}
+            p="24px 40px 32px "
+            textAlign={"center"}
+            bgColor={"white"}
+            mt="120px"
+          >
+            <Text
+              fontSize="22px"
+              fontWeight={"bold"}
+              textAlign={"left"}
+              color="teal.600"
+              fontFamily="Open Sauce One',sans-serif"
+            >
+              Belanja...
+            </Text>
+            <TableContainer>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th></Th>
+                    <Th></Th>
+                    <Th></Th>
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>{renderItem()}</Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Box>
+
+        <AlertDialog isOpen={isOpen} onClose={onClose}>
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontStyle="bold">
+                Cancel Order
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure to cancel this order?
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Batal
+                </Button>
+                <Button colorScheme="red" onClick={handleConfirmCancel}>
+                  Cancel
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Stack>
+    </>
+  )
+}
+
+export default Order
--- projects/server/routes/orderRoute.js
+++ projects/server/routes/orderRoute.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const orderController = require("../controllers/orderController")
 
-router.post("/cancel/:id", orderController.cancelOrder)
+router.patch("/cancel/:id", orderController.cancelOrder)
 router.get("/", orderController.getOrder)
 router.get("/:id", orderController.getOrderById)
 
