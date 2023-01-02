210a4b3b84e4486a36350b45bba55b7bf2973e2f
fraya <fitrah.fraya@gmail.com>
2023-01-03 02:30:57 +0700

    fixed

--- projects/server/routes/orderRoute.js
+++ projects/server/routes/orderRoute.js
@@ -2,8 +2,8 @@ const express = require("express")
 const router = express.Router()
 const orderController = require("../controllers/orderController")
 
-router.patch("/cancel/:id", orderController.cancelOrder)
-router.get("/", orderController.getOrder)
-router.get("/:id", orderController.getOrderById)
+// router.patch("/cancel/:id", orderController.cancelOrder)
+// router.get("/", orderController.getOrder)
+// router.get("/:id", orderController.getOrderById)
 
 module.exports = router
