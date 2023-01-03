70f60f40ebc2ab4e3146847f69f0d7aa25751cfb
ADLI <nuradlifathan@gmail.com>
2023-01-03 20:29:26 +0700

    bug

--- projects/client/src/components/cart/CartItem.jsx
+++ projects/client/src/components/cart/CartItem.jsx
@@ -1,4 +1,4 @@
-import { AddIcon, MinusIcon } from "@chakra-ui/icons"
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
 import {
   Box,
   CloseButton,
@@ -26,13 +26,13 @@ import {
   AlertDialogContent,
   AlertDialogHeader,
   AlertDialogFooter,
-} from "@chakra-ui/react"
-import { useEffect } from "react"
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
-import { axiosInstance } from "../../api"
-import { Rupiah } from "../../lib/currency/Rupiah"
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { axiosInstance } from "../../api";
+import { Rupiah } from "../../lib/currency/Rupiah";
 const CartItem = ({
   product_name,
   price,
@@ -45,110 +45,110 @@ const CartItem = ({
   totalHarga,
   onDelete,
 }) => {
-  const [productStock, setProductStock] = useState(0)
-  const [checkProduct, setCheckProduct] = useState(false)
-  const [qtyProduct, setQtyProduct] = useState(quantity)
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const cartSelector = useSelector((state) => state.cart)
-  const toast = useToast()
+  const [productStock, setProductStock] = useState(0);
+  const [checkProduct, setCheckProduct] = useState(false);
+  const [qtyProduct, setQtyProduct] = useState(quantity);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cartSelector = useSelector((state) => state.cart);
+  const toast = useToast();
 
   const btnDelete = () => {
-    onClose()
-    onDelete()
-  }
+    onClose();
+    onDelete();
+  };
   const fetchCartById = async () => {
     try {
-      const response = await axiosInstance.get(`/carts/${CartId}`)
+      const response = await axiosInstance.get(`/carts/${CartId}`);
 
       const cartProductStock = response.data.data.Product.ProductStocks.map(
         (val) => val.stock
-      )
-      let total = 0
+      );
+      let total = 0;
 
       for (let i = 0; i < cartProductStock.length; i++) {
-        total += Number(cartProductStock[i])
+        total += Number(cartProductStock[i]);
       }
-      setProductStock(total)
+      setProductStock(total);
 
       if (response.data.data.is_checked === true) {
-        setCheckProduct(true)
+        setCheckProduct(true);
       } else {
-        setCheckProduct(false)
+        setCheckProduct(false);
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
   const fetchCartByProduct = async () => {
     try {
       const response = await axiosInstance.get(
         `/carts/cart-product/ProductId/${productId}`
-      )
+      );
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const checkPerProduct = async () => {
     try {
       const response = await axiosInstance.patch(
         `/carts/productCheck/${CartId}`
-      )
+      );
       if (response.data.data.is_checked === true) {
-        setCheckProduct(true)
+        setCheckProduct(true);
       } else {
-        setCheckProduct(false)
+        setCheckProduct(false);
       }
-      totalHarga()
+      totalHarga();
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const addQty = async () => {
     try {
-      await axiosInstance.post(`/carts/addQty/${CartId}`)
-      fetchCartItem()
-      setQtyProduct(quantity + 1)
+      await axiosInstance.post(`/carts/addQty/${CartId}`);
+      fetchCartItem();
+      setQtyProduct(quantity + 1);
 
-      totalHarga()
-      fetchCartByProduct()
+      totalHarga();
+      fetchCartByProduct();
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
   const decQty = async () => {
     try {
-      await axiosInstance.patch(`/carts/decQty/${CartId}`)
-      fetchCartItem()
+      await axiosInstance.patch(`/carts/decQty/${CartId}`);
+      fetchCartItem();
       if (quantity <= 1) {
-        return 1
+        return 1;
       }
-      setQtyProduct(quantity - 1)
+      setQtyProduct(quantity - 1);
 
-      totalHarga()
-      fetchCartByProduct()
+      totalHarga();
+      fetchCartByProduct();
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
   const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
     useNumberInput({
       step: 1,
       defaultValue: qtyProduct,
       min: 1,
       max: qtyProduct,
-    })
-  const inc = getIncrementButtonProps(addQty)
-  const dec = getDecrementButtonProps(decQty)
-  const input = getInputProps(qtyProduct)
-  const qty = Number(input.value)
+    });
+  const inc = getIncrementButtonProps(addQty);
+  const dec = getDecrementButtonProps(decQty);
+  const input = getInputProps(qtyProduct);
+  const qty = Number(input.value);
 
   useEffect(() => {
-    fetchCartById()
-    fetchCartByProduct()
-    fetchCartItem()
-  }, [checkAllProduct, qtyProduct])
+    fetchCartById();
+    fetchCartByProduct();
+    fetchCartItem();
+  }, [checkAllProduct, qtyProduct]);
   return (
     <>
       <Flex
@@ -204,23 +204,28 @@ const CartItem = ({
         >
           <InputGroup w="40%">
             <InputLeftElement>
-            {/* <Button onClick={addQty} variant="unstyled" isDisabled={productStock <= qtyProduct} {...inc}> */}
+              {/* <Button onClick={addQty} variant="unstyled" isDisabled={productStock <= qtyProduct} {...inc}> */}
               <AddIcon
                 fontSize="10"
                 color={productStock <= qtyProduct ? "#c0cada" : "#0095DA"}
                 {...inc}
-                
               />
-            {/* </Button> */}
+              {/* </Button> */}
             </InputLeftElement>
-            <Input width="10em" textAlign="center" _hover={"none"} {...input} isDisabled={productStock === 0 ? true : false}/>
+            <Input
+              width="10em"
+              textAlign="center"
+              _hover={"none"}
+              {...input}
+              isDisabled={productStock === 0 ? true : false}
+            />
             <InputRightElement>
-            <Button variant="unstyled" {...dec}>
-            <MinusIcon
-                fontSize="10"
-                color={qtyProduct > 1 ? "#0095DA" : "#c0cada"}
-              />
-            </Button>
+              <Button variant="unstyled" {...dec}>
+                <MinusIcon
+                  fontSize="10"
+                  color={qtyProduct > 1 ? "#0095DA" : "#c0cada"}
+                />
+              </Button>
             </InputRightElement>
           </InputGroup>
           <HStack spacing="1">
@@ -243,16 +248,7 @@ const CartItem = ({
           <Link fontSize="sm" textDecor="underline" onClick={btnDelete}>
             Hapus Produk
           </Link>
-          {/* <Select
-            maxW="64px"
-            aria-label="Select quantity"
-            focusBorderColor={useColorModeValue("blue.500", "blue.200")}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-          </Select> */}
+
           <InputGroup w="40%">
             <InputLeftElement>
               <MinusIcon
@@ -272,7 +268,6 @@ const CartItem = ({
               />
             </InputRightElement>
           </InputGroup>
-          {/* <PriceTag price={price} currency={currency} /> */}
         </Flex>
       </Flex>
       <Divider />
@@ -299,7 +294,7 @@ const CartItem = ({
         </AlertDialogOverlay>
       </AlertDialog>
     </>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
