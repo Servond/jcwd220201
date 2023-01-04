33a168d7aa2075f6ea794bf31a46cdd36d2d40f7
fraya <fitrah.fraya@gmail.com>
2023-01-04 10:56:01 +0700

    bug fixing

--- projects/server/routes/orderRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const orderController = require("../controllers/orderController")
-
-// router.patch("/cancel/:id", orderController.cancelOrder)
-// router.get("/", orderController.getOrder)
-// router.get("/:id", orderController.getOrderById)
-
-module.exports = router
