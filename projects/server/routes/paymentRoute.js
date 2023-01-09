73ca023bc66e45612d7d153ebf4bb0f464b0a853
fraya <fitrah.fraya@gmail.com>
2023-01-09 23:26:20 +0700

    bug fixing
    - confirm and reject
    - filter report

--- projects/server/routes/paymentRoute.js
+++ projects/server/routes/paymentRoute.js
@@ -10,20 +10,6 @@ const {
 const { verifyToken } = require("../middlewares/authMiddleware")
 const router = express.Router()
 
-router.get("/", paymentController.getPayment)
-// router.get("/order", paymentController.getPayment)
-router.get("/:id", paymentController.getPaymentById)
-router.get("product-admin", productAdminController.getAllProduct)
-router.get("/", authController.getAllUser)
-router.get("/", warehousesController.getAllWarehouses)
-
-// get nearest warehouse
-router.get("/nearest_warehouse", verifyToken, findNearestWarehouse)
-
-// get cart items
-router.get("/cart", verifyToken, getCartItems)
-
-// router.get("/product", paymentController.getProductPayment)
 router.patch("/confirm/:id", paymentController.confirmPayment)
 router.patch("/reject/:id", paymentController.rejectPayment)
 
